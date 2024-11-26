f2c52a7fdeb2a5ca1d1ff770827976a7b7249c79
astrobotical <romarioburke190@gmail.com>
2024-11-25 22:40:15 -0500

    added the other stuff

--- navbar.php
+++ navbar.php
@@ -2,8 +2,8 @@
     <a href="index.php" class="nav-logo">Ecommerce Store</a>
     <ul class="nav-links">
         <li><a href="welcome.php">Welcome Page</a></li>
-        <li><a href="about.php">About Us</a></li>
-        <li><a href="catalog.php">Products</a>
+        <li><a href="aboutus.php">About Us</a></li>
+        <li><a href="products.php">Products</a>
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     </ul>
